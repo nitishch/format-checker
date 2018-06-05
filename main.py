06606fe808c48cbe0396c4bc9cc48ca23ba3d370
Nitish Chandra <nitishchandrachinta@gmail.com>
2018-06-04 22:47:47 -0700

    Configure logging in Heroku deployment

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import os
 from flask import Flask
 from flask import request
 app = Flask(__name__)
+# From https://gist.github.com/seanbehan/547f5fc599bde304c89694a98c102bab
+if 'DYNO' in os.environ:
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.ERROR)
+
 
 @app.route('/verify-results', methods=['GET'])
 def hello():
